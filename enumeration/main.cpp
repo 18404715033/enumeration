5293f054784e7799c3a111559ec3e50731297889
18404715033 <965630447@qq.com>
2015-12-09 18:54:34 +0800

    枚举法
    在c++环境下，首先定义一个枚举类型，并指定其枚举元素的值。声明枚举变量a和b，并为枚举变量a赋初值。再输出枚举常量，在进行枚举变量的赋值和输出
    。枚举变量的赋值运算。

--- enumeration/main.cpp
+++ enumeration/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    enum color
+    {
+        RED=3,
+        YELLOW=6,
+        BLUE=9
+    };
+    enum color a=RED;
+    color b;
+    cout<<"RED="<<RED<<endl;
+    cout<<"YELLOW="<<YELLOW<<endl;
+    cout<<"BLUE="<<BLUE<<endl;
+    b=a;
+    a=BLUE;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    b=BLUE;
+    cout<<"a<b="<<(a<b)<<endl;
     return 0;
 }
